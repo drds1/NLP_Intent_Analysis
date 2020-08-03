52240ea5e90617548b9432831654dd99af010a0f
dstarkey23 <david.starkey@vortexa.com>
2020-08-03 22:24:40 +0100

    refactor lstm code

--- script_NLP_Intent.py
+++ script_NLP_Intent.py
@@ -33,6 +33,109 @@ def load_embeddings(file):
     return embeddings_dictionary
 
 
+class lstm_fit():
+
+    def __init__(self):
+        self.x = None
+        self.word_embeddings = None
+        self.tokenizer = None
+        self.Xtrain = None
+        self.Xtest = None
+        self.ytest = None
+        self.ytrain = None
+        self.embedding_matrix = None
+        self.model_lstm = None
+
+    def tokenize(self):
+        tokenizer = keras.preprocessing.text.Tokenizer(num_words=100,
+                                                       filters="?!':;,.#$&()*+-<=>@[\\]^_`{|}~\t\n",
+                                                       lower=True, split=' ')
+
+        tokenizer.fit_on_texts(self.Xtrain)
+        self.tokenizer = tokenizer
+        # assign number to each word
+        self.Xtrain_sequence = tokenizer.texts_to_sequences(self.Xtrain)
+        # padd the sequences of short sentences with 0s so everything is the same length
+        self.Xtrain_sequence = keras.preprocessing.sequence.pad_sequences(self.Xtrain_sequence,
+                                                                     padding='post')
+
+        self.idx_word = dict(tokenizer.index_word)
+        self.num_words = len(idx_word) + 1
+
+
+
+    def load_word_embeddings(self, path = '../disaster_nlp/data/non_tracked/glove.6B.100d.txt'):
+        '''
+        sepcify path to e.g. glove word embeddings
+        :return:
+        '''
+        self.word_embeddings = dict(load_embeddings(path))
+
+    def match_word_embeddings(self):
+        '''
+        :return:
+        '''
+        embeddings_words = list(self.word_embeddings.keys())
+        wordvec_dim = embeddings_dict[embeddings_words[0]].shape[0]
+        embedding_matrix = np.zeros((num_words, wordvec_dim))
+        for i, word in self.idx_word.items():
+            print(i, word, embeddings_words.get(word, None))
+            # Look up the word embedding
+            vector = embeddings_words.get(word, None)
+            # Record in matrix
+            if vector is not None:
+                embedding_matrix[i, :] = vector
+        self.embedding_matrix = embedding_matrix
+
+
+    def setup_net(self, epochs = 5, batch_size = 128):
+        '''
+
+        :return:
+        '''
+        self.nlabels = len(self.ytrain[0])
+        wordvec_dim = self.embedding_matrix.shape[1]
+        model_lstm = keras.Sequential()
+        # initialise Ebedding layer num_words = len(idx_word) + 1 to deal with 0 padding
+        # input_length is the number of words ids per sample e.g 28
+        # NOT the sample size of the training data
+        # you do not need to supply that info
+        model_lstm.add(keras.layers.Embedding(input_dim=self.num_words,
+                                              input_length=self.Xtrain_sequence.shape[1],
+                                              output_dim=wordvec_dim,
+                                              weights=[self.embedding_matrix],
+                                              trainable=False,
+                                              mask_zero=True))
+
+        # words which are not in the pretrained embeddings (with value 0) are ignored
+        model_lstm.add(keras.layers.Masking(mask_value=0.0))
+
+        # Recurrent layer
+        model_lstm.add(keras.layers.LSTM(200, return_sequences=False))
+        model_lstm.add(keras.layers.Dropout(0.4))
+        # model_lstm.add(keras.layers.LSTM(28, return_sequences=True))
+        # model_lstm.add(keras.layers.Dropout(0.2))
+        # model_lstm.add(keras.layers.LSTM(28, return_sequences=True))
+        # model_lstm.add(keras.layers.Dropout(0.2))
+        # model_lstm.add(keras.layers.LSTM(28, return_sequences=False))
+
+        # Output layer
+        model_lstm.add(keras.layers.Dense(self.nlabels))
+        model_lstm.add(keras.layers.Activation('softmax'))
+
+        # Compile the model
+        model_lstm.compile(
+            optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+        # model summary
+        model_lstm.summary()
+
+        ##fit
+        model_lstm.fit(self.Xtrain_sequence, self.ytrain, epochs=epochs, batch_size=batch_size)
+        self.model_lstm = model_lstm
+
+
+#prepare data
 dftrain = pd.read_csv('./data/datasets_117486_281522_atis.train.csv')
 Xtrain, ytrain = list(dftrain.values[:,1]), list(dftrain.values[:,-1])
 Xtrain = stop_word_removal(Xtrain,words=['is', 'the', 'of', 'a'])
@@ -47,188 +150,32 @@ Xtest = [Xt.replace('BOS ','').replace(' EOS','') for Xt in Xtest]
 ytest = pd.get_dummies(ytest)
 ytest = np.array(ytest)
 
-#convert all abstracts to sequences of integers key stored in idx_word
-tokenizer = keras.preprocessing.text.Tokenizer(num_words=100,
-                                               filters="?!':;,.#$&()*+-<=>@[\\]^_`{|}~\t\n",
-                                               lower=True, split=' ')
 
+#load the lstm model and perform tokenisation and load word embeddings
+x = lstm_fit()
+x.Xtrain = Xtrain
+x.Xtest = Xtest
+x.ytrain = ytrain
+x.ytest = ytest
 
-tokenizer.fit_on_texts(Xtrain)
-word_counts = pd.DataFrame(dict(tokenizer.word_counts),index=['word count']).transpose()
-word_counts.sort_values(by='word count',ascending = False,inplace=True)
-print(word_counts.head(10))
-#assign number to each word
-Xtrain_sequence = tokenizer.texts_to_sequences(Xtrain)
-#padd the sequences of short sentences with 0s so everything is the same length
-Xtrain_sequence = keras.preprocessing.sequence.pad_sequences(Xtrain_sequence,
-                                                             padding='post')
+x.tokenize()
+x.load_word_embeddings()
+x.match_word_embeddings()
+x.setup_net()
 
-idx_word = tokenizer.index_word
-num_words = len(idx_word) + 1
-
-
-#load in word embeddings
-embeddings_dict = load_embeddings('../disaster_nlp/data/non_tracked/glove.6B.100d.txt')
-#embeddings_df = pd.DataFrame(embeddings_dict).transpose()
-#embeddings_df_cosine_similarity = cosine_similarity(embeddings_df)
-#embeddings_df['neural'].corrwith(embeddings_df)
-embeddings_words = list(embeddings_dict.keys())
-wordvec_dim = embeddings_dict[embeddings_words[0]].shape[0]
-embedding_matrix = np.zeros((num_words,wordvec_dim))
-for i, word in enumerate(idx_word.keys()):
-    # Look up the word embedding
-    vector = embeddings_dict.get(word, None)
-    # Record in matrix
-    if vector is not None:
-        embedding_matrix[i + 1, :] = vector
-
-
-#wordvec_dim = 100
-model_lstm = keras.Sequential()
-#initialise Ebedding layer num_words = len(idx_word) + 1 to deal with 0 padding
-#input_length is the number of words ids per sample e.g 28
-# NOT the sample size of the training data
-# you do not need to supply that info
-model_lstm.add(keras.layers.Embedding(input_dim=num_words,
-                                      input_length=Xtrain_sequence.shape[1],
-                                      output_dim=wordvec_dim,
-                                      weights=[embedding_matrix],
-                                      trainable=False,
-                                      mask_zero=True))
-
-#just test embedding layer
-#model_lstm.compile('rmsprop', 'mse')
-#embedding_test = model_lstm.predict(Xtrain_sequence)
-#Xtrain_sequence.shape ... (4274, 28)
-#embedding_test.shape ... (4274, 28. 100) i.e. each word converted to 100d vector
-
-
-#words which are not in the pretrained embeddings (with value 0) are ignored
-load_model = False
+#save to pickle
 picklefile = 'LSTM_model.pickle'
-
-model_lstm.add(keras.layers.Masking(mask_value = 0.0))
-
-# Recurrent layer
-model_lstm.add(keras.layers.LSTM(200, return_sequences=False))
-model_lstm.add(keras.layers.Dropout(0.4))
-#model_lstm.add(keras.layers.LSTM(28, return_sequences=True))
-#model_lstm.add(keras.layers.Dropout(0.2))
-#model_lstm.add(keras.layers.LSTM(28, return_sequences=True))
-#model_lstm.add(keras.layers.Dropout(0.2))
-#model_lstm.add(keras.layers.LSTM(28, return_sequences=False))
-
-
-# Dropout for regularisation and avoid overfit
-#model_lstm.add(keras.layers.Dropout(0.2))
-
-# Output layer
-model_lstm.add(keras.layers.Dense(nlabels))
-model_lstm.add(keras.layers.Activation('softmax'))
-
-# Compile the model
-model_lstm.compile(
-    optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-# model summary
-model_lstm.summary()
-
-##fit
-#model_lstm.fit(Xtrain_sequence, ytrain,epochs=5,batch_size=128)
-
-#save fitted model
 picklefile = picklefile
 os.system('rm ' + picklefile)
 pickle_out = open(picklefile, "wb")
-pickle.dump(model_lstm, pickle_out)
+pickle.dump(x, pickle_out)
 pickle_out.close()
 
 ## load previous simulation
-#pickle_in = open(picklefile, "rb")
-#model_lstm = pickle.load(pickle_in)
+pickle_in = open(picklefile, "rb")
+x = pickle.load(pickle_in)
 
-##predict values for testing
-Xtest_sequence = tokenizer.texts_to_sequences(Xtest)
-Xtest_sequence = keras.preprocessing.sequence.pad_sequences(Xtest_sequence,
-                                                            maxlen = Xtrain_sequence.shape[1],
-                                                            padding='post')
-
-
-
-
-
-
-#test
-model_lstm2 = keras.Sequential()
-#initialise Ebedding layer num_words = len(idx_word) + 1 to deal with 0 padding
-#input_length is the number of words ids per sample e.g 28
-# NOT the sample size of the training data
-# you do not need to supply that info
-'''
-CREATE A NEW TEMP NET WHICH IS JUST THE EMBEDDING LAYER
-JUST TO SEE WHAT COMES OUT
-#ALL OUTPUTS OF EMBEDDING LAYER ARE 0 THIS IS WHY THE NET FAILS!!
-'''
-model_lstm2.add(keras.layers.Embedding(input_dim=num_words,
-                                      input_length=Xtrain_sequence.shape[1],
-                                      output_dim=wordvec_dim,
-                                      weights=[embedding_matrix],
-                                      trainable=False,
-                                      mask_zero=True))
-model_lstm2.compile(
-    optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-# model summary
-model_lstm2.summary()
-
-
-#eval_lstm = model_lstm.evaluate(Xtest_sequence, ytest)
-ypred = model_lstm2.predict(Xtest_sequence)
-#ALL OUTPUTS OF EMBEDDING LAYER ARE 0 THIS IS WHY THE NET FAILS!!
-ypred_class = [np.argmax(yp) for yp in ypred]
-
-
-X0 = Xtrain_sequence[0,:]
-x00 = ''
-for x in X0:
-    x00+= idx_word.get(x,'')+' '
-print(x00)
-print(Xtrain[0])
-'''
-
-        input1 = Input(shape=(max_words,))
-        embedding_layer1 = Embedding(top_word, 100, weights=[embedding_matrix], input_length=max_words,
-                                     trainable=False)(input1)
-        dropout1 = Dropout(0.2)(embedding_layer1)
-        lstm1_1 = LSTM(128, return_sequences=True)(dropout1)
-        lstm1_2 = LSTM(128, return_sequences=True)(lstm1_1)
-        lstm1_2a = LSTM(128, return_sequences=True)(lstm1_2)
-        lstm1_3 = LSTM(128)(lstm1_2a)
-
-        input2 = Input(shape=(max_words_ky,))
-        embedding_layer2 = Embedding(top_word, 100, weights=[embedding_matrix], input_length=max_words_ky,
-                                     trainable=False)(
-            input2)
-        dropout2 = Dropout(0.2)(embedding_layer2)
-        lstm2_1 = LSTM(64, return_sequences=True)(dropout2)
-        lstm2_2 = LSTM(64, return_sequences=True)(lstm2_1)
-        lstm2_3 = LSTM(64)(lstm2_2)
-
-        input3 = Input(shape=(max_words_lc,))
-        embedding_layer3 = Embedding(top_word, 100, weights=[embedding_matrix], input_length=max_words_lc,
-                                     trainable=False)(
-            input3)
-        dropout3 = Dropout(0.2)(embedding_layer3)
-        lstm3_1 = LSTM(32, return_sequences=True)(dropout3)
-        lstm3_2 = LSTM(32, return_sequences=True)(lstm3_1)
-        lstm3_3 = LSTM(32)(lstm3_2)
-
-        merge = concatenate([lstm1_3, lstm2_3, lstm3_3])
-
-        dropout = Dropout(0.8)(merge)
-        dense1 = Dense(256, activation='relu')(dropout)
-        dense2 = Dense(128, activation='relu')(dense1)
-        output = Dense(2, activation='softmax')(dense2)
-        model = Model(inputs=[input1, input2, input3], outputs=output)
-        model.summary()
-'''
+model_lstm = x.model_lstm
+ypred_proba = model_lstm.predict(ytrain)
+ypred_class = [np.argmax(yp) for yp in ypred_proba]
+ypred_label = [labels[yp] for yp in ypred_class]
